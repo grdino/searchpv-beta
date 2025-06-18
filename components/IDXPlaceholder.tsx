5d5f5d9a922bf294d9e8bcef6b611396ae46044e
grdino <67813644+grdino@users.noreply.github.com>
2025-06-17 19:00:29 -0600

    Delete components directory

--- components/IDXPlaceholder.tsx
+++
@@ -1,8 +0,0 @@
-export default function IDXPlaceholder() {
-  return (
-    <section className="border-2 border-dashed p-8 text-center rounded-lg text-gray-500">
-      <h2 className="text-xl font-medium mb-2">Property Search Coming Soon</h2>
-      <p>MLS/IDX integration will be added here once approved and available.</p>
-    </section>
-  );
-}
